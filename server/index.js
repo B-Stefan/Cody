5c0982a01e5de1cb38e5501eb592614bbed581fb
Stefan Bieliauskas <stefan.bie@live.de>
2017-06-26 15:09:04 +0200

    - add default server startup

--- server/index.js
+++ server/index.js
@@ -1,34 +1,17 @@
-const http = require('http');
-const Bot = require('messenger-bot');
+const Server = require('./src/server');
 
-let bot = new Bot({
+let options = {
   token: 'PAGE_TOKEN',
   verify: 'VERIFY_TOKEN',
   app_secret: 'APP_SECRET',
-  graph_url: "http://localhost:3100/v2.6"
-});
+  graph_url: "https://graph.facebook.com/v2.6/"
+};
 
-bot.on('error', (err) => {
-  console.log(err.message)
-});
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text;
-
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err;
-
-    reply({ text }, (err) => {
-      if (err) throw err;
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
-  })
-});
-
-http.createServer(bot.middleware()).listen(3000, (err)=>{
-  if(err){
-    throw new Error(err)
-  }
-  console.log('Echo bot server running at port 3000.');
+const server = new Server(options);
+server.start().then(()=>{
+  console.log("Server started ");
+}).catch((err)=>{
+  console.error(err);
+  throw new Error(err);
 });
